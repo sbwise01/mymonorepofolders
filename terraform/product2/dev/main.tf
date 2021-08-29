12f61d176c2a8182aae2f9f6918061a4af586398
Brad Wise <>
2021-08-28 20:37:34 -0500

    Upgrade product2 in dev to 1.1.0

--- terraform/product2/dev/main.tf
+++ terraform/product2/dev/main.tf
@@ -1,5 +1,5 @@
 module "product2" {
-  source = "git@github.com:sbwise01/myproduct2module.git?ref=release/1.0.0"
+  source = "git@github.com:sbwise01/myproduct2module.git?ref=release/1.1.0"
 
   providers = {
     kubernetes = kubernetes.eks
